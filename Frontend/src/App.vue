1a5a038f603d2a3a85dd120886a3fa188505cc76
ox404fff <ox404fff@gmail.com>
2020-04-11 19:50:09 +0200

    Dev bootstrap

--- Frontend/src/App.vue
+++ Frontend/src/App.vue
@@ -1,12 +1,58 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <MainPage msg="Welcome to Your Vue.js App"/>
+    <b-navbar toggleable="lg" type="dark" variant="info">
+      <b-navbar-brand href="#">NavBar</b-navbar-brand>
+
+      <b-navbar-toggle target="nav-collapse"></b-navbar-toggle>
+
+      <b-collapse id="nav-collapse" is-nav>
+        <b-navbar-nav>
+          <b-nav-item href="#">Link</b-nav-item>
+          <b-nav-item href="#" disabled>Disabled</b-nav-item>
+        </b-navbar-nav>
+
+        <!-- Right aligned nav items -->
+        <b-navbar-nav class="ml-auto">
+          <b-nav-form>
+            <b-form-input size="sm" class="mr-sm-2" placeholder="Search"></b-form-input>
+            <b-button size="sm" class="my-2 my-sm-0" type="submit">Search</b-button>
+          </b-nav-form>
+
+          <b-nav-item-dropdown text="Lang" right>
+            <b-dropdown-item href="#">EN</b-dropdown-item>
+            <b-dropdown-item href="#">ES</b-dropdown-item>
+            <b-dropdown-item href="#">RU</b-dropdown-item>
+            <b-dropdown-item href="#">FA</b-dropdown-item>
+          </b-nav-item-dropdown>
+
+          <b-nav-item-dropdown right>
+            <!-- Using 'button-content' slot -->
+            <template v-slot:button-content>
+              <em>User</em>
+            </template>
+            <b-dropdown-item href="#">Profile</b-dropdown-item>
+            <b-dropdown-item href="#">Sign Out</b-dropdown-item>
+          </b-nav-item-dropdown>
+        </b-navbar-nav>
+      </b-collapse>
+    </b-navbar>
+    <div id="content">
+      <img alt="Vue logo" src="./assets/logo.png">
+      <MainPage msg="Welcome to Your Vue.js App"/>
+    </div>
   </div>
 </template>
 
 <script>
+import Vue from 'vue'
+import { BootstrapVue, IconsPlugin } from 'bootstrap-vue'
+import 'bootstrap/dist/css/bootstrap.css'
+import 'bootstrap-vue/dist/bootstrap-vue.css'
+
 import MainPage from './components/MainPage.vue'
+
+Vue.use(BootstrapVue)
+Vue.use(IconsPlugin)
 
 export default {
   name: 'App',
@@ -17,7 +63,7 @@ export default {
 </script>
 
 <style>
-#app {
+#content {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
--- Frontend/src/components/MainPage.vue
+++ Frontend/src/components/MainPage.vue
@@ -1,5 +1,13 @@
 <template>
-    <h1>{{ msg }}</h1>
+    <div>
+        <h1>{{ msg }}</h1>
+        <div>
+            <b-button>Button</b-button>
+            <b-button variant="danger">Button</b-button>
+            <b-button variant="success">Button</b-button>
+            <b-button variant="outline-primary">Button</b-button>
+        </div>
+    </div>
 </template>
 
 <script>
