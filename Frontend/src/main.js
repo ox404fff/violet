e7b2224709e6ba87c1c98c64dc38fbe4b5febd76
ox404fff <ox404fff@gmail.com>
2020-05-03 17:58:36 +0200

    Layout

--- Frontend/src/main.js
+++ Frontend/src/main.js
@@ -1,8 +1,17 @@
 import Vue from 'vue'
 import App from './App'
 import router from './router'
+import VueYandexMetrika from 'vue-yandex-metrika'
 
 Vue.config.productionTip = false;
+
+Vue.use(VueYandexMetrika, {
+  id: 62527144,
+  router: router,
+  env: process.env.NODE_ENV
+});
+
+console.log(process.env.NODE_ENV);
 
 new Vue({
   el: '#app',
